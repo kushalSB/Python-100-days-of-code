2c09aad02f8c1447f1d99227cd2555f2c5a7a5b4
Kushal-S-BAstakoti <sharmakushalbastakoti@gmail.com>
2022-08-02 11:12:00 +0545

    day001:band-name-generator-completed

---
+++ 001-Variables-Band-Name-Generator/main.py
@@ -0,0 +1,12 @@
+"""
+Band name Generator
+Inputs: Pet name and City name
+Output: Band name
+"""
+
+
+print("Welcome to Band Name Generator - Created by Kushal SB")
+city = input("Enter name of city: ")
+pet = input ("Enter name of pet: ")
+
+print (f"Your band name could be {city + pet}")
---
+++ README.md
@@ -0,0 +1,3 @@
+# Python 100 days of code
+
+## Day 1 Band Name Generator
